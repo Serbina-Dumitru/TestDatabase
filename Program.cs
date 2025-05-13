adee47c4401524af8d936933f6ecf411c3dd6fef
Serbina-Dumitru <serbina.dumitru@elev.cihcahul.md>
2025-05-13 06:00:01 +0300

    Tried to create get and some put requests, using newtonsoft for json to generate json responses

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using DbFunctionality;
+using Microsoft.EntityFrameworkCore;
+using TestDatabase;
+using Newtonsoft.Json;
 
 public class Program
 {
@@ -9,13 +12,39 @@ public class Program
     var app = builder.Build();
 
     app.UseHttpsRedirection();
-
     app.MapGet("/SayHi", () => {
         return "Hello";
     })
     .WithName("SayHiName");
 
-    DbFunctionalityClass.PrintAllTheDb();
+    app.MapGet("/GetAllUsers", () => {
+      using(var _context = new Context()){
+        var users =  _context.Users
+          .Include(u => u.SentMessages)
+          .Include(u => u.Notifications)
+          .Include(u => u.UsersInChats).ThenInclude(uc => uc.Chat)
+          .Include(u => u.Contacts).ThenInclude(c => c.ContactUser)
+          .Include(u => u.ContactedBy)
+          .ToList();
+        //var options = new JsonSerializerOptions{ ReferenceHandler = ReferenceHandler.IgnoreCycles};
+        //return Results.Json(users,options);
+        return JsonConvert.SerializeObject(users, Formatting.Indented,
+            new JsonSerializerSettings()
+            { 
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+        })
+    .WithName("GetAllUsers");
+
+    //app.MapPost("/login", async (string token) => {
+    //  using(var _context = new Context()){
+    //    var user = _context.Users.Select(u => u.SessionToken == token);
+    //    if(user.Count == 0) return 0;
+    //  }
+    //});
+
+    //DbFunctionalityClass.PrintAllTheDb();
     app.Run();
   }
 }

8f8da783a6b84e9664c3e149ce9a96cd0fd1be89
AlexandrBelocamenschi <belocamenschi.alexandr@elev.cihcahul.md>
2025-05-16 11:14:42 +0300

    Added user identification

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -8,25 +8,29 @@ namespace TestDatabase.Controllers
     [Route("[controller]")]
     public class LoginController : ControllerBase
     {
-    //     app.MapGet("/GetAllUsers", () => {
-    //   using(var _context = new Context()){
-    //     var users =  _context.Users
-    //       .Include(u => u.SentMessages)
-    //       .Include(u => u.Notifications)
-    //       .Include(u => u.UsersInChats).ThenInclude(uc => uc.Chat)
-    //       .Include(u => u.Contacts).ThenInclude(c => c.ContactUser)
-    //       .Include(u => u.ContactedBy)
-    //       .ToList();
-    //     //var options = new JsonSerializerOptions{ ReferenceHandler = ReferenceHandler.IgnoreCycles};
-    //     //return Results.Json(users,options);
-    //     return JsonConvert.SerializeObject(users, Formatting.Indented,
-    //         new JsonSerializerSettings()
-    //         { 
-    //         ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-    //         });
-    //     }
-    //     })
-    // .WithName("GetAllUsers");
+        public Context context = new Context();
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginInfo userLoginInfo)
+        {
+            if (string.IsNullOrWhiteSpace(userLoginInfo.Username) || string.IsNullOrWhiteSpace(userLoginInfo.Password))
+            {
+                return BadRequest(new { status = "error", error = "empty data" });
+            }
+
+            var user = await context.Users
+                .FirstOrDefaultAsync(u => u.Username == userLoginInfo.Username && u.Password == userLoginInfo.Password);
+
+            if (user == null)
+            {
+                return Unauthorized(new { status = "error", error = "user not found" });
+            }
+
+            return Ok(new { status = "success", data = new { user } });
+        }
+        public class UserLoginInfo{
+            public string Username {get; set;}
+            public string Password {get; set;}
+        }
         [HttpGet("GetAllUsers")]
         public IActionResult GetAllUsers(){
                 using(var _context = new Context()){

4cb5d687a85c90720cc8957da854125c6f9acdc0
Serbina-Dumitru <serbina.dumitru@elev.cihcahul.md>
2025-05-16 06:14:07 +0300

    Implemented the logic in theory should make the context scoped to request.

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -4,46 +4,31 @@ using Newtonsoft.Json;
 
 namespace TestDatabase.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class LoginController : ControllerBase
-    {
-    //     app.MapGet("/GetAllUsers", () => {
-    //   using(var _context = new Context()){
-    //     var users =  _context.Users
-    //       .Include(u => u.SentMessages)
-    //       .Include(u => u.Notifications)
-    //       .Include(u => u.UsersInChats).ThenInclude(uc => uc.Chat)
-    //       .Include(u => u.Contacts).ThenInclude(c => c.ContactUser)
-    //       .Include(u => u.ContactedBy)
-    //       .ToList();
-    //     //var options = new JsonSerializerOptions{ ReferenceHandler = ReferenceHandler.IgnoreCycles};
-    //     //return Results.Json(users,options);
-    //     return JsonConvert.SerializeObject(users, Formatting.Indented,
-    //         new JsonSerializerSettings()
-    //         { 
-    //         ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-    //         });
-    //     }
-    //     })
-    // .WithName("GetAllUsers");
-        [HttpGet("GetAllUsers")]
-        public IActionResult GetAllUsers(){
-                using(var _context = new Context()){
-            var users =  _context.Users
-            .Include(u => u.SentMessages)
-            .Include(u => u.Notifications)
-            .Include(u => u.UsersInChats).ThenInclude(uc => uc.Chat)
-            .Include(u => u.Contacts).ThenInclude(c => c.ContactUser)
-            .Include(u => u.ContactedBy)
-            .ToList();
-            string res = JsonConvert.SerializeObject(users, Formatting.Indented,
-            new JsonSerializerSettings()
-            { 
-            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-            });
-            return Ok(new {status = "OK", data = res});
-            }
-        }
-    }    
+  [ApiController]
+  [Route("[controller]")]
+  public class LoginController : ControllerBase
+  {
+    private readonly Context _context;
+
+    public LoginController(Context context){
+      _context = context;
+    }
+
+    [HttpGet("GetAllUsers")]
+    public IActionResult GetAllUsers(){
+      var users =  _context.Users
+        .Include(u => u.SentMessages)
+        .Include(u => u.Notifications)
+        .Include(u => u.UsersInChats).ThenInclude(uc => uc.Chat)
+        .Include(u => u.Contacts).ThenInclude(c => c.ContactUser)
+        .Include(u => u.ContactedBy)
+        .ToList();
+      string res = JsonConvert.SerializeObject(users, Formatting.Indented,
+          new JsonSerializerSettings()
+          { 
+          ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+          });
+      return Ok(new {status = "OK", data = res});
+    }
+  }    
 }
--- Program.cs
+++ Program.cs
@@ -9,61 +9,19 @@ public class Program
   {
     var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
-    DbFunctionalityClass.FillWithData();
+    builder.Services.AddDbContext<Context>(options => options.UseSqlite("Data Source = Messenger.db"),ServiceLifetime.Scoped);
+
     var app = builder.Build();
     app.MapControllers();
     app.UseHttpsRedirection();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
 
-    app.MapGet("/SayHi", () => {
-        return "Hello";
-    })
-    .WithName("SayHiName");
-
-    app.MapGet("/GetAllUsers", Program.GetAllUsersAsync).WithName("GetAllUsers");
-
-    app.MapPost("/login", Program.LogIn);
 
     DbFunctionalityClass.FillWithData();
     app.Run();
   }
-  
-  public static async Task<string> LogIn(string username, string password){
-    using(var _context = new Context()){
-      User? user = await _context.Users.FirstOrDefaultAsync<User>(u => u.Username == username);//.ToList();
 
-      if(user == null) return "Such an user does not exists";
-      if(user.Password != password) return "The password is incorect";
-
-      string generatingToken = "SomeRandomTokenThatShouldBeGenerated";
-      user.SessionToken = generatingToken;
-      user.SessionTokenExpirationDate = DateTime.Now.AddDays(1);
-      try{ await _context.SaveChangesAsync(); }
-      catch(DbUpdateException){
-        return "Something went wrong, unable to update the database, contact the developer.";
-      }
-
-      return generatingToken;
-    }
-    return "Something wen really wrong on server side whether you see this.";
-  } 
-
-  public static async Task<string> GetAllUsersAsync()
-  {
-    using(var _context = new Context()){
-      var users =  await _context.Users
-        .Include(u => u.SentMessages)
-        .Include(u => u.Notifications)
-        .Include(u => u.UsersInChats).ThenInclude(uc => uc.Chat)
-        .Include(u => u.Contacts).ThenInclude(c => c.ContactUser)
-        .Include(u => u.ContactedBy)
-        .ToListAsync();
-      return JsonConvert.SerializeObject(users, Formatting.Indented,
-          new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }
-          );
-    }
-  }
 }

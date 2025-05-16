766ada7dd336241166843fc21bcac787e9a26f8e
AlexandrBelocamenschi <belocamenschi.alexandr@elev.cihcahul.md>
2025-05-16 11:14:42 +0300

    Added user identification

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -4,31 +4,50 @@ using Newtonsoft.Json;
 
 namespace TestDatabase.Controllers
 {
-  [ApiController]
-  [Route("[controller]")]
-  public class LoginController : ControllerBase
-  {
-    private readonly Context _context;
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        public Context context = new Context();
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginInfo userLoginInfo)
+        {
+            if (string.IsNullOrWhiteSpace(userLoginInfo.Username) || string.IsNullOrWhiteSpace(userLoginInfo.Password))
+            {
+                return BadRequest(new { status = "error", error = "empty data" });
+            }
 
-    public LoginController(Context context){
-      _context = context;
-    }
+            var user = await context.Users
+                .FirstOrDefaultAsync(u => u.Username == userLoginInfo.Username && u.Password == userLoginInfo.Password);
 
-    [HttpGet("GetAllUsers")]
-    public IActionResult GetAllUsers(){
-      var users =  _context.Users
-        .Include(u => u.SentMessages)
-        .Include(u => u.Notifications)
-        .Include(u => u.UsersInChats).ThenInclude(uc => uc.Chat)
-        .Include(u => u.Contacts).ThenInclude(c => c.ContactUser)
-        .Include(u => u.ContactedBy)
-        .ToList();
-      string res = JsonConvert.SerializeObject(users, Formatting.Indented,
-          new JsonSerializerSettings()
-          {
-          ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-          });
-      return Ok(new {status = "OK", data = res});
-    }
-  }
+            if (user == null)
+            {
+                return Unauthorized(new { status = "error", error = "user not found" });
+            }
+
+            return Ok(new { status = "success", data = new { user } });
+        }
+        public class UserLoginInfo{
+            public string Username {get; set;}
+            public string Password {get; set;}
+        }
+        [HttpGet("GetAllUsers")]
+        public IActionResult GetAllUsers(){
+                using(var _context = new Context()){
+            var users =  _context.Users
+            .Include(u => u.SentMessages)
+            .Include(u => u.Notifications)
+            .Include(u => u.UsersInChats).ThenInclude(uc => uc.Chat)
+            .Include(u => u.Contacts).ThenInclude(c => c.ContactUser)
+            .Include(u => u.ContactedBy)
+            .ToList();
+            string res = JsonConvert.SerializeObject(users, Formatting.Indented,
+            new JsonSerializerSettings()
+            { 
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+            return Ok(new {status = "OK", data = res});
+            }
+        }
+    }    
 }
